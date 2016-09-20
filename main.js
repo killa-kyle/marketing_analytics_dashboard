473d1ea2dda81923448a7eea85dc0bf3d8d9af58
killa-kyle <kylerose@gmail.com>
2016-09-20 18:28:47 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,534 @@
+gapi.analytics.ready(function() {
+
+  /**
+   * Authorize the user immediately if the user has already granted access.
+   * If no access has been created, render an authorize button inside the
+   * element with the ID "embed-api-auth-container".
+   */
+  gapi.analytics.auth.authorize({
+    container: 'embed-api-auth-container',
+    clientid: '476592068856-ki5v355o2t1pthp0h73s6bgddm3v0i80.apps.googleusercontent.com'
+  });
+
+
+  /**
+   * Create a new ActiveUsers instance to be rendered inside of an
+   * element with the id "active-users-container" and poll for changes every
+   * five seconds.
+   */
+  var activeUsers = new gapi.analytics.ext.ActiveUsers({
+    container: 'active-users-container',
+    pollingInterval: 5
+  });
+
+
+  /**
+   * Add CSS animation to visually show the when users come and go.
+   */
+  activeUsers.once('success', function() {
+    var element = this.container.firstChild;
+    var timeout;
+
+    this.on('change', function(data) {
+      var element = this.container.firstChild;
+      var animationClass = data.delta > 0 ? 'is-increasing' : 'is-decreasing';
+      element.className += (' ' + animationClass);
+
+      clearTimeout(timeout);
+      timeout = setTimeout(function() {
+        element.className =
+            element.className.replace(/ is-(increasing|decreasing)/g, '');
+      }, 3000);
+    });
+  });
+
+
+  /**
+   * Create a new ViewSelector2 instance to be rendered inside of an
+   * element with the id "view-selector-container".
+   */
+  var viewSelector = new gapi.analytics.ext.ViewSelector2({
+    container: 'view-selector-container',
+  })
+  .execute();
+
+
+  /**
+   * Update the activeUsers component, the Chartjs charts, and the dashboard
+   * title whenever the user changes the view.
+   */
+  viewSelector.on('viewChange', function(data) {
+    var title = document.getElementById('view-name');
+    title.innerHTML = data.property.name + ' (' + data.view.name + ')';
+
+    // Start tracking active users for this view.
+    activeUsers.set(data).execute();
+
+    // Render all the of charts for this view.
+    renderWeekOverWeekChart(data.ids);
+    renderYearOverYearChart(data.ids);
+    renderTopBrowsersChart(data.ids);
+    renderTopChannelsChart(data.ids);
+
+
+  });
+
+
+  /*
+  * Register plugin for displaying labels
+  */
+  Chart.pluginService.register({
+    beforeRender: function (chart) {
+      if (chart.config.options.showAllTooltips) {
+        // create an array of tooltips
+        // we can't use the chart tooltip because there is only one tooltip per chart
+        chart.pluginTooltips = [];
+        chart.config.data.datasets.forEach(function (dataset, i) {
+          chart.getDatasetMeta(i).data.forEach(function (sector, j) {
+            chart.pluginTooltips.push(new Chart.Tooltip({
+              _chart: chart.chart,
+              _chartInstance: chart,
+              _data: chart.data,
+              _options: chart.options,
+              _active: [sector]
+            }, chart));
+          });
+        });
+
+        // turn off normal tooltips
+        chart.options.tooltips.enabled = false;
+      }
+    },
+    afterDraw: function (chart, easing) {
+      if (chart.config.options.showAllTooltips) {
+        // we don't want the permanent tooltips to animate, so don't do anything till the animation runs atleast once
+        if (!chart.allTooltipsOnce) {
+          if (easing !== 1)
+            return;
+          chart.allTooltipsOnce = true;
+        }
+
+        // turn on tooltips
+        chart.options.tooltips.enabled = true;
+        Chart.helpers.each(chart.pluginTooltips, function (tooltip) {
+          tooltip.initialize();
+          tooltip.update();
+          // we don't actually need this since we are not animating tooltips
+          tooltip.pivot();
+          tooltip.transition(easing).draw();
+        });
+        chart.options.tooltips.enabled = false;
+      }
+    }
+  })
+
+
+  /**
+   * Draw the a chart.js line chart with data from the specified view that
+   * overlays session data for the current week over session data for the
+   * previous week.
+   */
+  function renderWeekOverWeekChart(ids) {
+
+    // Adjust `now` to experiment with different days, for testing only...
+    var now = moment(); // .subtract(3, 'day');
+
+    var thisWeek = query({
+      'ids': ids,
+      'dimensions': 'ga:date,ga:nthDay',
+      'metrics': 'ga:sessions',
+      'start-date': moment(now).subtract(1, 'day').day(0).format('YYYY-MM-DD'),
+      'end-date': moment(now).format('YYYY-MM-DD')
+    });
+
+    var lastWeek = query({
+      'ids': ids,
+      'dimensions': 'ga:date,ga:nthDay',
+      'metrics': 'ga:sessions',
+      'start-date': moment(now).subtract(1, 'day').day(0).subtract(1, 'week')
+          .format('YYYY-MM-DD'),
+      'end-date': moment(now).subtract(1, 'day').day(6).subtract(1, 'week')
+          .format('YYYY-MM-DD')
+    });
+
+    Promise.all([thisWeek, lastWeek]).then(function(results) {
+
+      var data1 = results[0].rows.map(function(row) { return +row[2]; });
+      var data2 = results[1].rows.map(function(row) { return +row[2]; });
+      var labels = results[1].rows.map(function(row) { return +row[0]; });      
+      labels = labels.map(function(label) {
+        return moment(label, 'YYYYMMDD').format('ddd');
+      });
+
+      // replace null future dates this week with 0 so chart displays
+      for (var i = 0; i < 7; i++) {
+        if(data1[i] == null || data1[i] == undefined){
+          data1[i] = 0;
+        }
+      }
+
+
+      var data = {
+        labels : labels,
+        datasets : [
+          {
+            label: 'Last Week',
+            fillColor : 'rgba(220,220,220,0.5)',
+            strokeColor : 'rgba(220,220,220,1)',
+            pointColor : 'rgba(220,220,220,1)',
+            pointStrokeColor : '#fff',
+            data : data2
+          },
+          {
+            label: 'This Week',
+            fillColor : 'rgba(151,187,205,0.5)',
+            strokeColor : 'rgba(151,187,205,1)',
+            pointColor : 'rgba(151,187,205,1)',
+            pointStrokeColor : '#fff',
+            data : data1
+          }
+        ]
+      };
+
+      new Chart(makeCanvas('chart-1-container')).Bar(data);
+      generateLegend('legend-1-container', data.datasets);
+    });
+  }
+
+
+  /**
+   * Draw the a chart.js bar chart with data from the specified view that
+   * overlays session data for the current year over session data for the
+   * previous year, grouped by month.
+   */
+  function renderYearOverYearChart(ids) {
+
+    // Adjust `now` to experiment with different days, for testing only...
+    var now = moment(); // .subtract(3, 'day');
+
+    var thisYear = query({
+      'ids': ids,
+      'dimensions': 'ga:month,ga:nthMonth',
+      'metrics': 'ga:users',
+      'start-date': moment(now).date(1).month(0).format('YYYY-MM-DD'),
+      'end-date': moment(now).format('YYYY-MM-DD')
+    });
+
+    var lastYear = query({
+      'ids': ids,
+      'dimensions': 'ga:month,ga:nthMonth',
+      'metrics': 'ga:users',
+      'start-date': moment(now).subtract(1, 'year').date(1).month(0)
+          .format('YYYY-MM-DD'),
+      'end-date': moment(now).date(1).month(0).subtract(1, 'day')
+          .format('YYYY-MM-DD')
+    });
+
+    Promise.all([thisYear, lastYear]).then(function(results) {
+      var data1 = results[0].rows.map(function(row) { return +row[2]; });
+      var data2 = results[1].rows.map(function(row) { return +row[2]; });
+      var labels = ['Jan','Feb','Mar','Apr','May','Jun',
+                    'Jul','Aug','Sep','Oct','Nov','Dec'];
+
+      // Ensure the data arrays are at least as long as the labels array.
+      // Chart.js bar charts don't (yet) accept sparse datasets.
+      for (var i = 0, len = labels.length; i < len; i++) {
+        if (data1[i] === undefined) data1[i] = null;
+        if (data2[i] === undefined) data2[i] = null;
+      }
+
+      var data = {
+        labels : labels,
+        datasets : [
+          {
+            label: 'Last Year',
+            fillColor : 'rgba(220,220,220,0.5)',
+            strokeColor : 'rgba(220,220,220,1)',
+            data : data2
+          },
+          {
+            label: 'This Year',
+            fillColor : 'rgba(151,187,205,0.5)',
+            strokeColor : 'rgba(151,187,205,1)',
+            data : data1
+          }
+        ]
+      };
+
+      new Chart(makeCanvas('chart-2-container')).Bar(data);
+      generateLegend('legend-2-container', data.datasets);
+    })
+    .catch(function(err) {
+      console.error(err.stack);
+    });
+  }
+
+
+  /**
+   * Draw the a chart.js doughnut chart with data from the specified view that
+   * show the top 5 browsers over the past seven days.
+   */
+  function renderTopBrowsersChart(ids) {
+
+    query({
+      'ids': ids,
+      'dimensions': 'ga:browser',
+      'metrics': 'ga:pageviews',
+      'sort': '-ga:pageviews',
+      'max-results': 5
+    })
+    .then(function(response) {
+
+      var data = [];
+      var colors = ['#4D5360','#949FB1','#D4CCC5','#E2EAE9','#F7464A'];
+
+      response.rows.forEach(function(row, i) {
+        data.push({ value: +row[1], color: colors[i], label: row[0] });
+      });
+
+      new Chart(makeCanvas('chart-3-container')).Doughnut(data);
+      generateLegend('legend-3-container', data);
+    });
+  }
+
+
+  /**
+   * Draw the a chart.js doughnut chart with data from the specified view that
+   * compares sessions from mobile, desktop, and tablet over the past seven
+   * days.
+   */
+  function renderTopChannelsChart(ids) {
+    // Adjust `now` to experiment with different days, for testing only...
+    var now = moment(); // .subtract(3, 'day');
+
+
+
+    // This week 
+    var thisWeek = query({
+      'ids': ids,
+      'dimensions': 'ga:channelGrouping',
+      'metrics': 'ga:sessions,ga:goal1ConversionRate,ga:goal1Completions',
+      'sort': '-ga:sessions',
+      'start-date': moment(now).subtract(1, 'day').day(0).format('YYYY-MM-DD'),
+      'end-date': moment(now).format('YYYY-MM-DD'),
+      'max-results': 7
+    });
+
+    var lastWeek = query({
+      'ids': ids,
+      'dimensions': 'ga:channelGrouping',
+      'metrics': 'ga:sessions',
+      'sort': '-ga:sessions',
+      'start-date': moment(now).subtract(1, 'day').day(0).subtract(1, 'week')
+          .format('YYYY-MM-DD'),
+      'end-date': moment(now).subtract(1, 'day').day(6).subtract(1, 'week')
+          .format('YYYY-MM-DD'),
+      'max-results': 7
+    });
+
+    var lastMonth = query({
+      'ids': ids,
+      'dimensions': 'ga:channelGrouping',
+      'metrics': 'ga:sessions',
+      'sort': '-ga:sessions',
+      'start-date': moment(now).subtract(1,'months').startOf('month').format('YYYY-MM-DD'),
+      'end-date': moment(now).subtract(1, 'months').endOf('month').format('YYYY-MM-DD'),
+      'max-results': 7
+    });
+    console.log(thisWeek);
+    Promise.all([thisWeek, lastWeek,lastMonth]).then(function(results) {
+
+      var thisWeekResults = results[0].rows.map(function(row){return row});
+      var lastWeekResults = results[1].rows.map(function(row){return row});
+      var lastMonthResults = results[2].rows.map(function(row){return row});
+
+
+      // console.log(thisWeekResults);
+      // console.log(lastWeekResults);
+
+      // build multi-array to fill table 
+      var tableResults = new Array(3);
+      tableResults[0] = thisWeekResults;
+      tableResults[1] = lastWeekResults;
+      tableResults[2] = lastMonthResults;
+      
+
+
+      var data = [];
+      var colors = ['#4D5360','#949FB1','#D4CCC5','#E2EAE9','#F7464A','#bada55','#e6711b'];
+
+
+
+      tableResults[0].forEach(function(row, i) {
+        // $('#channels').append('<tr>'+ 
+        //   '<td>'+row[0]+'</td>'+
+        //       '<td>'+row[1]+'</td>'+
+        //       '<td></td>'+
+        //       '<td></td>'+
+        //   '</tr>');
+
+      
+        data.push({
+          label: row[0],
+          value: +row[1],
+          color: colors[i]
+        });
+      });
+
+      for(var i = 0; i < tableResults[0].length; i++){
+        console.log('Channel Name:',tableResults[0][i][1].toLocaleString('en-US'));
+        var channelName = tableResults[0][i][0];
+        var thisWeek = parseInt(tableResults[0][i][1]).toLocaleString();
+        // var thisWeekRate = parseFloat(tableResults[0][i][2]);
+        var thisWeekCompletions = parseInt(tableResults[0][i][3]).toLocaleString();
+        var lastWeek = parseInt(tableResults[1][i][1]).toLocaleString();
+        var lastMonth = parseInt(tableResults[2][i][1]).toLocaleString();
+        $('#channels').append('<tr class="channel-row">'+ 
+          '<td>'+channelName+'</td>'+
+              '<td>'+thisWeek+'<span>('+thisWeekCompletions+')(thisWeekRate)</span></td>'+
+              '<td>'+lastWeek+'</td>'+
+              '<td>'+lastMonth+'</td>'+
+          '</tr>');
+      }
+      // lastWeekResults.forEach(function(row, i) {
+      //   $('#channels tr:nth-child('+i+') td:nth-child(3)').html(row[1]);
+      //   // $('#channels tr td:nth-child(3)').html(row[1]);
+      // });
+     
+     // build total row
+      $('#channels').append(
+        '<tr>'+
+                '<td><strong>TOTAL:</strong></td>'+
+                '<td></td>'+
+                '<td></td>'+
+                '<td></td>'+
+        '</tr>');
+      new Chart(makeCanvas('chart-4-container')).Doughnut(data);
+      generateLegend('legend-4-container', data);
+    });
+  }
+
+
+
+
+
+  function ChannelBreakdownChart(ids) {
+
+    // Adjust `now` to experiment with different days, for testing only...
+    var now = moment(); // .subtract(3, 'day');
+
+    var thisWeek = query({
+      'ids': ids,
+      'dimensions': 'ga:date,ga:nthDay',
+      'metrics': 'ga:sessions',
+      'start-date': moment(now).subtract(1, 'day').day(0).format('YYYY-MM-DD'),
+      'end-date': moment(now).format('YYYY-MM-DD')
+    });
+
+    var lastWeek = query({
+      'ids': ids,
+      'dimensions': 'ga:date,ga:nthDay',
+      'metrics': 'ga:sessions',
+      'start-date': moment(now).subtract(1, 'day').day(0).subtract(1, 'week')
+          .format('YYYY-MM-DD'),
+      'end-date': moment(now).subtract(1, 'day').day(6).subtract(1, 'week')
+          .format('YYYY-MM-DD')
+    });
+
+    Promise.all([thisWeek, lastWeek]).then(function(results) {
+
+      var data1 = results[0].rows.map(function(row) { return +row[2]; });
+      var data2 = results[1].rows.map(function(row) { return +row[2]; });
+      var labels = results[1].rows.map(function(row) { return +row[0]; });
+
+      labels = labels.map(function(label) {
+        return moment(label, 'YYYYMMDD').format('ddd');
+      });
+
+      var data = {
+        labels : labels,
+        datasets : [
+          {
+            label: 'Last Week',
+            fillColor : 'rgba(220,220,220,0.5)',
+            strokeColor : 'rgba(220,220,220,1)',
+            pointColor : 'rgba(220,220,220,1)',
+            pointStrokeColor : '#fff',
+            data : data2
+          },
+          {
+            label: 'This Week',
+            fillColor : 'rgba(151,187,205,0.5)',
+            strokeColor : 'rgba(151,187,205,1)',
+            pointColor : 'rgba(151,187,205,1)',
+            pointStrokeColor : '#fff',
+            data : data1
+          }
+        ]
+      };
+
+      new Chart(makeCanvas('chart-1-container')).Line(data);
+      generateLegend('legend-1-container', data.datasets);
+    });
+  }
+
+
+  /**
+   * Extend the Embed APIs `gapi.analytics.report.Data` component to
+   * return a promise the is fulfilled with the value returned by the API.
+   * @param {Object} params The request parameters.
+   * @return {Promise} A promise.
+   */
+  function query(params) {
+    return new Promise(function(resolve, reject) {
+      var data = new gapi.analytics.report.Data({query: params});
+      data.once('success', function(response) { resolve(response); })
+          .once('error', function(response) { reject(response); })
+          .execute();
+    });
+  }
+
+
+  /**
+   * Create a new canvas inside the specified element. Set it to be the width
+   * and height of its container.
+   * @param {string} id The id attribute of the element to host the canvas.
+   * @return {RenderingContext} The 2D canvas context.
+   */
+  function makeCanvas(id) {
+    var container = document.getElementById(id);
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+
+    container.innerHTML = '';
+    canvas.width = container.offsetWidth;
+    canvas.height = container.offsetHeight;
+    container.appendChild(canvas);
+
+    return ctx;
+  }
+
+
+  /**
+   * Create a visual legend inside the specified element based off of a
+   * Chart.js dataset.
+   * @param {string} id The id attribute of the element to host the legend.
+   * @param {Array.<Object>} items A list of labels and colors for the legend.
+   */
+  function generateLegend(id, items) {
+    var legend = document.getElementById(id);
+    legend.innerHTML = items.map(function(item) {
+      var color = item.color || item.fillColor;
+      var label = item.label;
+      return '<li><i style="background:' + color + '"></i>' + label + '</li>';
+    }).join('');
+  }
+
+
+  // Set some global Chart.js defaults.
+  Chart.defaults.global.animationSteps = 60;
+  Chart.defaults.global.animationEasing = 'easeInOutQuart';
+  Chart.defaults.global.responsive = true;
+  Chart.defaults.global.maintainAspectRatio = false;
+
+});
