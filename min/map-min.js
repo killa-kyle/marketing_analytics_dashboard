d4232cc3c453cf152efe631d1751e2d7d74920f9
killa-kyle <kylerose@gmail.com>
2016-09-21 15:03:04 -0400

    active pages + active map

---
+++ min/map-min.js
@@ -0,0 +1,64 @@
+var ACTIVE_USERS_MAP = (function(){
+    var active_users_map = {}
+    var geo_data;
+      
+      active_users_map.init= function(data){
+        $('#regions_div').html('test');
+        geo_data= data;
+        google.charts.load('upcoming', {'packages':['geochart']});
+        google.charts.setOnLoadCallback(active_users_map.drawRegionsMap);
+      },
+      active_users_map.drawRegionsMap = function(){
+
+          // var data = google.visualization.arrayToDataTable([
+          //   ["40.712784", "-74.005936"],
+          //   ["31.459017", "-85.640289"]
+          //   // ['United States', 300],
+          //   // ['Brazil', 400],
+          //   // ['Canada', 500],
+          //   // ['France', 600],
+          //   // ['RU', 700]
+          // ]);
+
+          var data = new google.visualization.DataTable();
+           data.addColumn('number', 'LATITUDE');                                
+           data.addColumn('number', 'LONGITUDE');
+           data.addColumn('string', 'DESCRIPTION'); 
+           data.addColumn('number', 'Value:', 'value'); 
+           data.addColumn({type:'string', role:'tooltip'});                        
+
+          // data.addRows([[40.712784, -74.005936, 'First marker', 0,'tooltip']]);
+          // data.addRows([[ 31.459017,-85.640289, 'Second Marker', 1,'tooltip']]);
+
+          for (var i = geo_data.length - 1; i >= 0; i--) {
+            var LATITUDE = parseFloat(geo_data[i][2]);
+            var LONGITUDE= parseFloat(geo_data[i][3]);
+            data.addRows([[LATITUDE,LONGITUDE, geo_data[i][4], i,geo_data[i][5]]]);
+          }
+
+
+          var options = {
+            colorAxis: {colors: ['#e7711c', '#4374e0']}, // orange to blue
+            legend: 'none',    
+            // backgroundColor: {fill:'transparent',stroke:'#FFF' ,strokeWidth:0 },    
+            datalessRegionColor: '#f5f5f5',
+            displayMode: 'markers', 
+            enableRegionInteractivity: 'true', 
+            resolution: 'countries',
+            sizeAxis: {minValue: 1, maxValue:10,minSize:5,  maxSize: 10},
+            region:'auto',
+            keepAspectRatio: true,
+            tooltip: {textStyle: {color: '#444444'}}    
+            };
+
+          var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+          chart.draw(data, options);
+        
+      }
+
+    return active_users_map;
+}());
+
+
+
